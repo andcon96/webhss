192cc3f98a4931690f0b9fda612c3824c127adcc
andcon96 <78584931+andcon96@users.noreply.github.com>
2022-06-30 10:09:25 +0700

    Update 063022

--- app/Models/Transaksi/ReportBiaya.php
+++ app/Models/Transaksi/ReportBiaya.php
@@ -3,8 +3,10 @@
 namespace App\Models\Transaksi;
 
 use App\Models\Master\Truck;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class ReportBiaya extends Model
 {
@@ -17,4 +19,19 @@ class ReportBiaya extends Model
         return $this->belongsTo(Truck::class, 'rb_truck_id', 'id');
     }
     
+    
+    protected static function boot()
+    {
+        parent::boot();
+        
+        self::creating(function($model){
+            $model->rb_domain = Session::get('domain');
+        });
+
+        self::addGlobalScope(function(Builder $builder){
+            // $builder->where('user_id', '=', Auth()->user()->id);
+            $builder->where('rb_domain', Session::get('domain'));
+            $builder->orderBy('created_at','DESC');
+        });
+    }
 }
