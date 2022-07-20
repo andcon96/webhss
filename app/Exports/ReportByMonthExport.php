040a7e58f559bf4679e7738780b71705eb8f0db0
andcon96 <78584931+andcon96@users.noreply.github.com>
2022-07-20 15:56:33 +0700

    tst

--- app/Exports/ReportByMonthExport.php
+++ app/Exports/ReportByMonthExport.php
@@ -33,31 +33,35 @@ class ReportByMonthExport implements FromView, WithColumnWidths, ShouldAutoSize,
         $nopol = $truckcol->truck_no_polis;
 
         $data = SuratJalan::query();
+        $openSPK = SuratJalan::query();
         $rbhist = ReportBiaya::query();
 
         if($datefrom){
             $data->where('sj_eff_date','>=',$datefrom);
+            $openSPK->where('sj_eff_date','>=',$datefrom);
             $rbhist->where('rb_eff_date','>=',$datefrom);
         }
         if($dateto){
             $data->where('sj_eff_date','<=',$dateto);
+            $openSPK->where('sj_eff_date','<=',$dateto);
             $rbhist->where('rb_eff_date','<=',$dateto);
         }
         if($truck){
             $data->where('sj_truck_id',$truck);
+            $openSPK->where('sj_truck_id',$truck);
             $rbhist->where('rb_truck_id','>=',$truck);
         }
 
         $rbhist = $rbhist->with('getTruck')->get();
 
-        // $openSPK = $data->with('getDetail.getItem',
-        //                         'getSOMaster.getCOMaster.getCustomer',
-        //                         'getSOMaster.getShipFrom',
-        //                         'getSOMaster.getShipTo',
-        //                         'getRuteHistory.getRute',
-        //                         )
-        //                     ->where('sj_status','!=','Closed')    
-        //                     ->get();
+        $openSPK = $openSPK->with('getDetail.getItem',
+                                'getSOMaster.getCOMaster.getCustomer',
+                                'getSOMaster.getShipFrom',
+                                'getSOMaster.getShipTo',
+                                'getRuteHistory.getRute',
+                                )
+                            ->where('sj_status','!=','Closed')    
+                            ->get();
 
         $data = $data->with('getDetail.getItem',
                             'getSOMaster.getCOMaster.getCustomer',
@@ -71,7 +75,7 @@ class ReportByMonthExport implements FromView, WithColumnWidths, ShouldAutoSize,
                             
         $totalrb = ReportBiaya::where('rb_truck_id',$truck)->sum('rb_nominal');
 
-        // dd($data, $openSPK);
+        dd($data, $openSPK);
 
         return view('transaksi.laporan.excel.report-date-range',
                         compact('data','datefrom','dateto','nopol','totalrb','rbhist'));
