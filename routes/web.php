098f87e3fae393d065046911bf43cd87d8059a68
andcon96 <78584931+andcon96@users.noreply.github.com>
2022-10-03 16:54:06 +0700

    comment load rute

--- routes/web.php
+++ routes/web.php
@@ -57,11 +57,11 @@ Route::get('/', function () {
 Auth::routes();
 
 // Route::get('loadrutefirst',[RuteController::class,'loadrutefirst']);
-Route::get('loadhistoryrute',[RuteController::class,'loadhistoryrute']);
+// Route::get('loadhistoryrute',[RuteController::class,'loadhistoryrute']);
 // Route::get('loadinvoicefirst',[InvoiceMTController::class,'loadinvoicefirst']);
-Route::get('loadinvoice',[InvoiceMTController::class,'loadinvoice']);
-Route::get('loadinvoicecontainer',[InvoiceMTController::class,'loadinvoicecontainer']);
-Route::get('loadhistoryrutedetail',[RuteController::class,'loadhistoryrutedetail']);
+// Route::get('loadinvoice',[InvoiceMTController::class,'loadinvoice']);
+// Route::get('loadinvoicecontainer',[InvoiceMTController::class,'loadinvoicecontainer']);
+// Route::get('loadhistoryrutedetail',[RuteController::class,'loadhistoryrutedetail']);
 
 Route::get('newloadhistoryrute',[RuteController::class,'newloadhistoryrute']);
 Route::get('newloadhistorydetail',[RuteController::class,'newloadhistoryrutedetail']);
