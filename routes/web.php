99af33c74384f49c759c28c1c63321985049d595
Michael-IMI <michael@ptimi.co.id>
2022-12-05 12:25:07 +0700

    aktivasi fungsi load

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Auth::routes();
 // Route::get('loadhistoryrute',[RuteController::class,'loadhistoryrute']);
 // Route::get('loadinvoicefirst',[InvoiceMTController::class,'loadinvoicefirst']);
 // Route::get('loadinvoice',[InvoiceMTController::class,'loadinvoice']);
-// Route::get('loadinvoicecontainer',[InvoiceMTController::class,'loadinvoicecontainer']);
+Route::get('loadinvoicecontainer',[InvoiceMTController::class,'loadinvoicecontainer']);
 // Route::get('loadhistoryrutedetail',[RuteController::class,'loadhistoryrutedetail']);
 // Route::get('newloadhistoryrute',[RuteController::class,'newloadhistoryrute']);
 // Route::get('newloadhistorydetail',[RuteController::class,'newloadhistoryrutedetail']);
