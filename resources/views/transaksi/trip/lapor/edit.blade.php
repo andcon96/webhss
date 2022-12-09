cecdb19b0a82f9ec49bbb08228da808d743c394a
Michael-IMI <michael@ptimi.co.id>
2022-12-09 15:11:39 +0700

    #10

--- resources/views/transaksi/trip/lapor/edit.blade.php
+++ resources/views/transaksi/trip/lapor/edit.blade.php
@@ -66,7 +66,7 @@
             <div class="offset-md-1 col-md-10" style="margin-top:90px;">
                 <div class="float-right">
                     {{-- <a href="{{Route('laportrip.index',['truck' => $data->sj_truck_id])}}" id="btnback" class="btn btn-success bt-action">Back</a> --}}
-                    @if($userdriver)
+                    @if($userDriver)
                     <a href="{{Route('laportrip.index',['truck' => $data->sj_truck_id])}}" id="btnback" class="btn btn-success bt-action">Back</a>
                     @else
                     <a href="{{Route('laportrip.index',['nomorso' => $data->sj_so_mstr_id])}}" id="btnback" class="btn btn-success bt-action">Back</a>
