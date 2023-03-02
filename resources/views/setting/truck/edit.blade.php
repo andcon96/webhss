6960df7914897d5002b33f147956aeefe970aab8
andcon96 <andrew.conan1996@gmail.com>
2023-03-02 15:04:44 +0700

    Revisi Edit Truck

--- resources/views/setting/truck/edit.blade.php
+++ resources/views/setting/truck/edit.blade.php
@@ -28,7 +28,8 @@
             <div class="form-group row">
                 <label for="subdomain" class="col-md-3 col-form-label text-md-right">{{ __('Sub Domain') }}</label>
                 <div class="col-md-7">
-                    <select id="subdomain" class="form-control subdomain" name="subdomain" autofocus required autocomplete="off">
+                    <select id="subdomain" class="form-control subdomain" name="subdomain" autofocus autocomplete="off">
+                            <option value="">Select Data</option>
                         @foreach($subdomain as $sd)
                             <option value="{{$sd->sd_sub_domain_code}}" {{$sd->sd_sub_domain_code == $data->truck_sub_domain ? 'Selected' : ''}}>{{$sd->sd_sub_domain_code}}</option>
                         @endforeach
