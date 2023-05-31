77fe1ac1eaa62cf65c77c87581557e93815aa978
= <=>
2023-06-01 02:52:12 +0700

    dd2

--- app/Http/Controllers/Transaksi/KerusakanLaporMTController.php
+++ app/Http/Controllers/Transaksi/KerusakanLaporMTController.php
@@ -398,7 +398,7 @@ class KerusakanLaporMTController extends Controller
     public function upassignkr($id, Request $request)
     {
         // $this->authorize('custompolicy',[KerusakanMstr::class]);
-        dd($request->all());
+ 
         $nopol = $request->truck;
         $wonbr = $request->sonbr;
         $gandeng = $request->gandengan;
@@ -409,6 +409,7 @@ class KerusakanLaporMTController extends Controller
         $gandengcode = $request->gandengcode;
         // validasi approval ada atau tidak
         $emailto = approval::get();
+        dd($emailto,is_null($emailto));
         if(is_null($emailto)){
             alert()->error('Error','Harap setting terlebih dahulu email untuk Approver di Approval Maintenance');
             return redirect()->route('laporkerusakan.index');
